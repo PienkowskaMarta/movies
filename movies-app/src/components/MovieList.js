b60f4001a4814e0ee76351a6a1618f277f493182
pienkowskamarta <marta.pienkowska@gmail.com>
2021-11-12 14:38:59 +0100

    list of results limited to 10

--- movies-app/src/components/MovieList.js
+++ movies-app/src/components/MovieList.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 
 const MovieList = (props) => {
-	const FavouriteComponent = props.favouriteComponent;
+  const FavouriteComponent = props.favouriteComponent;
 
-	return (
-		<>
-			{props.movies.map((movie, index) => (
-				<div>
-					<img src={movie.Poster} alt='movie'></img>
-					<div onClick={() => props.handleFavouritesClick(movie)}>
-						<FavouriteComponent />
-					</div>
-				</div>
-			))}
-		</>
-	);
+  return (
+    <>
+      {props.movies.slice(0, 10).map((movie, index) => (
+        <div>
+          <img src={movie.Poster} alt="movie"></img>
+          <div onClick={() => props.handleFavouritesClick(movie)}>
+            <FavouriteComponent />
+          </div>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default MovieList;
